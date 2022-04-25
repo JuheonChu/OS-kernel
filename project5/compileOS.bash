bee2b5844b9b6ff3606408a4aa05535924a736b6
JuheonChu <35699839+JuheonChu@users.noreply.github.com>
2022-04-25 19:54:52 -0400

    Add files via upload

--- project5/compileOS.bash
+++ project5/compileOS.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #refresh the path setting
 source ../PathSetter.bash
 
@@ -15,6 +16,14 @@ dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc seek=0
 dd if=map.img of=floppya.img bs=512 count=1 seek=1 conv=notrunc
 dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
 
+#copy the kernel file into the disk image at sector 3 
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+
+
+#place the provided message.txt file into sector 30 of the disk after copying in the kernel.
+dd if=message.txt of=floppya.img bs=512 conv=notrunc seek=30
+
+
 #compile kernel.c, uprog1.c, uprog2.c, shell.c, userLib.c, textEd.c
 bcc -ansi -c -o kernel.o kernel.c
 bcc -ansi -c -o uprog1.o uprog1.c
@@ -30,19 +39,13 @@ as86 lib.asm -o lib.o
 
 
 #link the object files and place the resulting executable file on the disk
-ld86 -o kernel -d kernel.o kernel_asm.o
+ld86 -o kernel -d kernel.o proc.o userLib.o kernel_asm.o
 ld86 -o uprog1 -d uprog1.o lib.o
 ld86 -o uprog2 -d uprog2.o lib.o 
 ld86 -o shell -d shell.o userLib.o lib.o
 ld86 -o textEd -d textEd.o userLib.o lib.o
-ld86 -o proc -d proc.o kernel_asm.o
 
 
-#copy the kernel file into the disk image at sector 3 
-dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
-
-#place the provided message.txt file into sector 30 of the disk after copying in the kernel.
-dd if=message.txt of=floppya.img bs=512 conv=notrunc seek=30
 
 # compile with gcc compiler for loadFile.c
 gcc -o loadFile loadFile.c
