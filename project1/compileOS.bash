3dfb5a25a79ca10e00ae4c993c05b764da4358b2
JuheonChu <35699839+JuheonChu@users.noreply.github.com>
2022-05-05 22:12:05 -0400

    Update compileOS.bash

--- project1/compileOS.bash
+++ project1/compileOS.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
-#refresh the setting
+#refresh the setting.
 source ../PathSetter.bash
-#compile the kernel file
+#compile the kernel file.
 bcc -ansi -c -o kernel.o kernel.c
-#assemble the kernel.asm file
+#assemble the kernel.asm file.
 as86 kernel.asm -o kernel_asm.o
-#link the kernel.o and kernel.asm files into the executable kernel file
+#link the kernel.o and kernel.asm files into the executable kernel file.
 ld86 -o kernel -d kernel.o kernel_asm.o
-#copy the kernel file into the disk image at sector 3 where the bootloader is expecting to find it
+#copy the kernel file into the disk image at sector 3 where the bootloader is expecting to find it by using dd utility.
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
-#run bochs to allow the bootloader to load the kernel 
+#run bochs to allow the bootloader to load the kernel.
 bochs -f opsys.bxrc
